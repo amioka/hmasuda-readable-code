e9613e40f407e4be06658c1913e4470d29a9b0a7
masuda <masuda@weic.jp>
2014-08-11 11:50:08 +0900

    Modify to use recipe_data_filename with command line argument

--- kadai/init.php
+++ kadai/init.php
@@ -1,5 +1,10 @@
 <?php
+if ($argc < 2)
+{
+    print "Usage: $argv[0] <recipe_data_filepath>\n";
+    exit(1);
+}
 
 // データファイル名
-define("DATAFILE", 'recipe_data.txt');
+define("DATAFILE", $argv[1]);
 
